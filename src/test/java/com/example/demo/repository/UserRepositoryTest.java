da6781038e60ab315ae473750b5e2ada371ebe58
jiae <hirlawldo413@gmail.com>
2020-08-03 19:06:33 +0900

    [feat] user repository test (CRUD test)

--- src/test/java/com/example/demo/repository/UserRepositoryTest.java
+++ src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -1,12 +1,17 @@
 package com.example.demo.repository;
 
+import com.example.demo.DemoApplicationTests;
 import com.example.demo.model.entity.User;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
-public class UserRepositoryTest {
+public class UserRepositoryTest extends DemoApplicationTests {
 
     @Autowired
     private UserRepository userRepository;
@@ -23,20 +28,45 @@ public class UserRepositoryTest {
         user.setPhoneNumber("010-1111-1111");
         user.setCreatedAt(LocalDateTime.now());
         user.setCreatedBy("admin");
-
         User newUser = userRepository.save(user); // 레포지토리에도 id도 추가해서 자동으로 생성됨.
         System.out.println("newUser : " + newUser);
     }
 
-    public void read(){
-
+    @Test
+    public void read(@RequestParam Long id){
+        Optional<User> user = userRepository.findById(2L); //TODO: 아이디를 받아오기
+        user.ifPresent(selectUser->{
+            System.out.println("user : " + selectUser);
+            System.out.println("email: " + selectUser.getEmail());
+        });
     }
 
+    @Test
     public void update(){
+        Optional<User> user = userRepository.findById(1L); //TODO: 아이디를 받아오기
+        user.ifPresent(selectUser->{
+            selectUser.setAccount("PPPP");
+            selectUser.setUpdatedAt(LocalDateTime.now());
+            selectUser.setUpdatedBy("update method()");
 
+            userRepository.save(selectUser);
+        });
     }
 
-    public void remove(){
+    @Test
+    @Transactional // 데이터를 지우지 않고 테스트 후 롤백해줌
+    public void delete(){
+        Optional<User> user = userRepository.findById(10L); //TODO: 아이디를 받아오기
+
+        Assert.assertTrue(user.isPresent()); // true
+
+        user.ifPresent(selectUser->{
+            userRepository.delete(selectUser);
+        });
+
+        Optional<User> deletedUser = userRepository.findById(10L); //TODO: 아이디를 받아오기
+
+        Assert.assertFalse(deletedUser.isPresent()); // false
 
     }
 }
