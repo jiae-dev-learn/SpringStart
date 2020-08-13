3309bf7e70cc1e4a02283acb9826c4fa301ed1d8
jiae <hirlawldo413@gmail.com>
2020-08-13 16:50:13 +0900

    [feat] userCreate 서비스

--- src/main/java/com/example/demo/controller/api/UserApiController.java
+++ src/main/java/com/example/demo/controller/api/UserApiController.java
@@ -4,17 +4,25 @@ import com.example.demo.controller.ifs.CrudInterface;
 import com.example.demo.model.network.Header;
 import com.example.demo.model.network.request.UserApiRequest;
 import com.example.demo.model.network.response.UserApiResponse;
+import com.example.demo.service.UserApiLogicService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-
+@Slf4j // simple logging for 4 java? 롬복에서 로그를 사용할 수 있는 부분.
 @RestController
 @RequestMapping("/api/user")
 public class UserApiController implements CrudInterface<UserApiRequest, UserApiResponse> {
 
+    @Autowired
+    private UserApiLogicService userApiLogicService;
+
     @Override
     @PostMapping("") // /api/user
-    public Header<UserApiResponse> create(@RequestBody Header<UserApiRequest> userApiRequest) {
-        return null;
+    public Header<UserApiResponse> create(@RequestBody Header<UserApiRequest> request) {
+        log.info("{}",request);
+
+        return userApiLogicService.create(request);
     }
 
     @Override
@@ -25,7 +33,7 @@ public class UserApiController implements CrudInterface<UserApiRequest, UserApiR
 
     @Override
     @PutMapping("") // /api/user
-    public Header<UserApiResponse> update(@RequestBody Header<UserApiRequest> userApiRequest) {
+    public Header<UserApiResponse> update(@RequestBody Header<UserApiRequest> request) {
         return null;
     }
 
--- src/main/java/com/example/demo/service/UserApiLogicService.java
+++ src/main/java/com/example/demo/service/UserApiLogicService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.controller.ifs.CrudInterface;
+import com.example.demo.model.entity.User;
 import com.example.demo.model.network.Header;
 import com.example.demo.model.network.request.UserApiRequest;
 import com.example.demo.model.network.response.UserApiResponse;
@@ -8,18 +9,33 @@ import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 public class UserApiLogicService implements CrudInterface<UserApiRequest, UserApiResponse> {
 
     @Autowired
     private UserRepository userRepository;
 
-    // 1. request data
-    // 2. user 생성
-    // 3. 생성된 데이터 -> UserApiResponse return
     @Override
-    public Header<UserApiResponse> create(Header<UserApiRequest> userApiRequest) {
-        return null;
+    public Header<UserApiResponse> create(Header<UserApiRequest> request) {
+        // 1. request data
+        UserApiRequest userApiRequest = request.getData();
+
+        // 2. user 생성
+        User user = User.builder()
+                .account(userApiRequest.getAccount())
+                .password(userApiRequest.getPassword())
+                .status("REGISTERED")
+                .phoneNumber(userApiRequest.getPhoneNumber())
+                .email(userApiRequest.getEmail())
+                .registeredAt(LocalDateTime.now())
+                .build();
+
+        User newUser = userRepository.save(user);
+
+        // 3. 생성된 데이터 -> UserApiResponse return
+        return response(newUser);
     }
 
     @Override
@@ -28,12 +44,29 @@ public class UserApiLogicService implements CrudInterface<UserApiRequest, UserAp
     }
 
     @Override
-    public Header<UserApiResponse> update(Header<UserApiRequest> userApiRequest) {
+    public Header<UserApiResponse> update(Header<UserApiRequest> request) {
         return null;
     }
 
     @Override
     public Header delete(Long id) {
         return null;
+    }
+
+    private Header<UserApiResponse> response(User user){
+        // user -> userApiResponse
+        UserApiResponse userApiResponse = UserApiResponse.builder()
+                .id(user.getId())
+                .account(user.getAccount())
+                .password(user.getPassword()) // TODO: 암호화, 길이
+                .email(user.getEmail())
+                .phoneNumber(user.getPhoneNumber())
+                .status(user.getStatus())
+                .registeredAt(user.getRegisteredAt())
+                .unregisteredAt(user.getUnregisteredAt())
+                .build();
+
+        //Header + data return
+        return Header.OK(userApiResponse);
     }
 }
